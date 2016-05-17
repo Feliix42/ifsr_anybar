f480b0f521b2dcd3d62530c498c1c975ca4a21af
Felix Wittwer <dev@felixwittwer.de>
2016-05-17 19:38:44 +0200

    Error Handling for 'API' call

--- src/main.rs
+++ src/main.rs
@@ -1,13 +1,22 @@
 extern crate hyper;
 
 use hyper::Client;
+use hyper::status::StatusCode;
 use std::io::Read;
 
 fn get_buerostatus() -> Option<bool> {
     let client = Client::new();
     let url = "https://www.ifsr.de/buerostatus/output.php";
 
-    let mut res = client.get(url).send().unwrap();
+    let mut res = match client.get(url).send() {
+        Ok(resp) => resp,
+        Err(_)   => return None,
+    };
+
+    // Check if response from Server is Status 200.
+    if res.status != StatusCode::Ok {
+        return None;
+    }
 
     let mut onezero = String::new();
     res.read_to_string(&mut onezero).unwrap();
@@ -16,7 +25,10 @@ fn get_buerostatus() -> Option<bool> {
     match onezero.as_ref() {
         "0" => Some(false),
         "1" => Some(true),
-        _   => None
+        _   => {
+            println!("Unknown return value from ifsr.de!");
+            None
+        }
     }
 }
 
